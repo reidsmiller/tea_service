80cdb68e63f9de0974bbc60f3ec4f295aa14d302
Reid Miller <reid.s.miller@gmail.com>
2023-07-25 11:02:04 -0600

    remove spec/examples.txt

--- spec/models/subscription_teas_spec.rb
+++ spec/models/subscription_teas_spec.rb
@@ -5,4 +5,9 @@ RSpec.describe SubscriptionTea, type: :model do
     it { should belong_to(:subscription) }
     it { should belong_to(:tea) }
   end
+
+  describe 'validations' do
+    it { should validate_presence_of(:subscription_id) }
+    it { should validate_presence_of(:tea_id) }
+  end
 end
--- spec/models/subscriptions_spec.rb
+++ spec/models/subscriptions_spec.rb
@@ -4,8 +4,8 @@ RSpec.describe Subscription, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:title) }
     it { should validate_presence_of(:price) }
-    it { should validate_numericality_of(:status) }
-    it { should validate_numericality_of(:frequency) }
+    it { should validate_presence_of(:status) }
+    it { should validate_presence_of(:frequency) }
   end
 
   describe 'relationships' do
