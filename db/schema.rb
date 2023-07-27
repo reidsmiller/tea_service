9775b8e0858c45dc8be1a89916b3f9142364a987
Reid Miller <reid.s.miller@gmail.com>
2023-07-27 10:08:49 -0600

    test: Add new subscription test

--- db/schema.rb
+++ db/schema.rb
@@ -26,7 +26,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_27_154100) do
   create_table "subscriptions", force: :cascade do |t|
     t.string "title"
     t.string "price"
-    t.integer "status"
+    t.integer "status", default: 0
     t.integer "frequency"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
